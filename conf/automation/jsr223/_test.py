800ba355c80b5af57b4c1677e3679de08d9be746
Mikael Andersen <andersen.mikael@gmail.com>
2020-11-08 05:19:05 +0100

    Adding old openhab config

--- conf/automation/jsr223/_test.py
+++
@@ -1,9 +0,0 @@
-from custom.helper import sendNotification, sendCommand
-
-#sendNotification("test","hallo")
-
-#sendNotification("test","hallo holger", recipient='bot1')
-
-#sendNotification("test","hallo sandra", recipient='bot2')
- 
-#sendCommand("Light_FF_Utilityroom_Ceiling",REFRESH) 
--- conf/transform/washer_runtime.js
+++
@@ -1,5 +0,0 @@
-(function(i) {
-    if( i == "-" || i == "NULL" || i == 0 ) return "0 min.";
- 
-    return Math.round(i / 60) + " min.";
-})(input)
